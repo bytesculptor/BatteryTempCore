6e1043f1382bc60357e7de2e0c3f5268b9314171
Byte_Sculptor <bytesculptor@gmail.com>
2021-02-07 22:11:15 +0000

    moving to and integration of BatteryTempCore lib

---
+++ src/main/java/com/bytesculptor/batterytempcore/help/OpenSourceActivity.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2017 - 2021  Byte Sculptor Software  - All Rights Reserved
+ *
+ * All information contained herein is and remains the property of Byte Sculptor Software.  
+ * Unauthorized copying of this file, via any medium, is strictly prohibited unless prior 
+ * written permission is obtained from Byte Sculptor Software.
+ *
+ * Romeo Rondinelli - bytesculptor@gmail.com
+ *
+ */
+
+package com.bytesculptor.batterytempcore.help
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.bytesculptor.batterytempcore.R
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+
+class OpenSourceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_open_source)
+        val more = findViewById<Button>(R.id.btMoreLicenses)
+        more.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View) {
+                startActivity(Intent(applicationContext, OssLicensesMenuActivity::class.java))
+            }
+        })
+    }
+}
---
+++ src/main/java/com/bytesculptor/batterytempcore/widget/providers/AllAppWidgetProvider.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2017 - 2021  Byte Sculptor Software  - All Rights Reserved
+ *
+ * All information contained herein is and remains the property of Byte Sculptor Software.
+ * Unauthorized copying of this file, via any medium, is strictly prohibited unless prior
+ * written permission is obtained from Byte Sculptor Software.
+ *
+ * Romeo Rondinelli - bytesculptor@gmail.com
+ *
+ */
+
+package com.bytesculptor.batterytempcore.widget.providers;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Dummy
+ */
+public class AllAppWidgetProvider extends AppWidgetProvider {
+
+}
